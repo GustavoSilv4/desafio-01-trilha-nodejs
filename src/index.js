ab2ec860293dded9fef7fa853d256777a5f120a8
GustavoSilv4 <GustavoSilv4.contato@gmail.com>
2022-07-05 10:30:47 -0300

    feat: added functionality to create a new task and search all tasks for a user

--- src/index.js
+++ src/index.js
@@ -11,11 +11,27 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
 
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "Not Found User!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
+
+  const usernameIsExists = users.some(user => user.username === username);
+
+  if (usernameIsExists) {
+    return response.status(400).json({ error: 'Exists user account!' });
+  }
 
   const newUser = {
     id: uuidv4(),
@@ -30,10 +46,28 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
 
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const newTodo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(newTodo);
+
+  return response.status(201).send(newTodo);
 
 });
 
