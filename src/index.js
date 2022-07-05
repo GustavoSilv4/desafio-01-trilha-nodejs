3eafca0df2d499a82edbc77048104aa6a59b6b10
GustavoSilv4 <GustavoSilv4.contato@gmail.com>
2022-07-05 10:57:08 -0300

    feat: added request to delete a task

--- src/index.js
+++ src/index.js
@@ -109,6 +109,19 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo Not Found!' });
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(200).send();
 
 });
 
